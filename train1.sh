991192d8c5f9a144695901773b9a6c94596b3f21
crazyxiaoxi <751251272@qq.com>
2025-10-21 06:41:54 +0000

    ir modality

--- train1.sh
+++ train1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 IMGSIZE=224
-BATCHSIZE=24
-CUDADEVICES=1,2,3
-MODALITY='rgbt'
-EPOCHS=110
+BATCHSIZE=4
+CUDADEVICES=2,3,6,7
+MODALITY='ir'
+EPOCHS=1
 echo -e "\n\n===================== 启动全部训练与测试 ====================="
 
 # echo -e "\n\n===== 启动 CLIPVG 训练与测试 ====="
@@ -14,13 +14,13 @@ echo -e "\n\n===== 启动 MDETR 训练与测试 ====="
 bash ./script_train/MDETR/mdetr_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
 
 echo -e "\n\n===== 启动 MMCA 训练与测试 ====="
-# bash ./script_train/MMCA_VG/mmca_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
+bash ./script_train/MMCA_VG/mmca_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
 
 echo -e "\n\n===== 启动 TRANSVG 训练与测试 ====="
-# bash ./script_train/TRANS_VG/transvg_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
+bash ./script_train/TRANS_VG/transvg_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
 
 echo -e "\n\n===== 启动 HIVG 训练与测试 ====="
-# bash ./script_train/HiVG/hivg_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
+bash ./script_train/HiVG/hivg_train_script_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES $EPOCHS
 
 # ---- 结束 ----
 echo -e "\n\n===================== 所有任务已执行完毕 ====================="
