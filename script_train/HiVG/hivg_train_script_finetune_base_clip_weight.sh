b92c80a4167fc364193000fd4ef0ec690c48ac48
crazyxiaoxi <751251272@qq.com>
2025-10-28 18:20:57 +0000

    test

--- script_train/HiVG/hivg_train_script_finetune_base_clip_weight.sh
+++ script_train/HiVG/hivg_train_script_finetune_base_clip_weight.sh
@@ -19,13 +19,13 @@ mkdir -p logs/hivg/$MODALITY
 # ================= 串行训练四个数据集 =================
 
 echo "===== Start FLIR training ====="
-stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_flir/hivg_single_dataset_flir_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/flir_train.log
+stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_flir/hivg_single_dataset_flir_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/$IMGSIZE"_"$BATCHSIZE"_flir.log"
 
 echo "===== Start M3FD training ====="
-stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_m3fd/hivg_single_dataset_m3fd_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/m3fd_train.log
+stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_m3fd/hivg_single_dataset_m3fd_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/$IMGSIZE"_"$BATCHSIZE"_m3fd.log"
 
 echo "===== Start MFAD training ====="
-stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_mfad/hivg_single_dataset_mfad_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/mfad_train.log
+stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_mfad/hivg_single_dataset_mfad_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/$IMGSIZE"_"$BATCHSIZE"_mfad.log"
 
 echo "===== Start MIXUP training ====="
-stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_mixup/hivg_mixup_dataset_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/mixup_train.log
+stdbuf -oL -eL bash ./script_train/HiVG/rgbtvg_mixup/hivg_mixup_dataset_finetune_base_clip_weight.sh $IMGSIZE $BATCHSIZE $MODALITY $CUDADEVICES 2>&1 | tee logs/hivg/$MODALITY/$IMGSIZE"_"$BATCHSIZE"_mixup.log"
